b9470853e45b24a7a6e19d685284be20a7b6fe70
kavyansh.pandey <kavyanshpandey27@gmail.com>
2022-09-13 09:56:00 +0530

    Removed dependency code from db.py

--- db.py
+++ db.py
@@ -11,12 +11,3 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
